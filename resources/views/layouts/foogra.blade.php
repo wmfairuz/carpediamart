1f47faaadb232eff3eb8195c61e3c74d02f7c51e
Fairuz Wan Ismail <fairuz.ismail@nazrol.tech>
2020-05-06 12:12:48 +0800

    Add FB messenger chat plugin

--- resources/views/layouts/foogra.blade.php
+++ resources/views/layouts/foogra.blade.php
@@ -55,6 +55,32 @@
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 </head>
 <body>
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v7.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+     attribution=setup_tool
+     page_id="586706101466984"
+     theme_color="#67b868"
+     logged_in_greeting="Hi, welcome to Carpedia Mart! Please let us know how can we help you."
+     logged_out_greeting="Hi, welcome to Carpedia Mart! Please let us know how can we help you.">
+</div>
 <header class="header clearfix element_to_stick">
     @include('header')
 </header>
