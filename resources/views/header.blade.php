e626d0aecbc53932cb4bfaa09bcb68a54f2fe8ca
Fairuz Wan Ismail <fairuz.ismail@nazrol.tech>
2020-05-06 12:12:33 +0800

    Update header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -6,7 +6,7 @@
         </a>
     </div>
     <ul id="top_menu">
-        <li><a href="#sign-in-dialog" id="sign-in" class="login">Contact Us +6013-6252699</a></li>
+{{--        <li><a href="#sign-in-dialog" id="sign-in" class="login">Contact Us +6013-6252699</a></li>--}}
         {{--            <li><a href="wishlist.html" class="wishlist_bt_top" title="Your wishlist">Your wishlist</a></li>--}}
     </ul>
     <!-- /top_menu -->
@@ -21,14 +21,17 @@
             <a href="index.html"><img src="{{ asset('img/logo.png') }}" width="140" height="35" alt=""></a>
         </div>
         <ul>
-            <li class="submenu">
-                <a href="#0" class="show-submenu">Home</a>
+            <li>
+                <a href="#">Home</a>
             </li>
-            <li class="submenu">
-                <a href="#0" class="show-submenu">The Carpedia e-Hailing</a>
+            <li>
+                <a href="#">The Carpedia e-Hailing</a>
             </li>
-            <li class="submenu">
-                <a href="#0" class="show-submenu">The Carpedia Academy</a>
+            <li>
+                <a href="#">The Carpedia Academy</a>
+            </li>
+            <li>
+                <a href="#">Contact Us +6019-577 8108</a>
             </li>
         </ul>
     </nav>
