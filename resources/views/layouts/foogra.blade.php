265bb56c45e10ca4b0bd4ad8fe616053fe7b42a3
Fairuz Wan Ismail <fairuz.ismail@nazrol.tech>
2020-10-15 10:11:32 +0800

    Add GTM

--- resources/views/layouts/foogra.blade.php
+++ resources/views/layouts/foogra.blade.php
@@ -7,6 +7,14 @@
     <meta name="description" content="CarpediaMart - Shop and deliver to your doorstep with a few clicks">
     <meta name="author" content="Fairuz">
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-M74WKHN');</script>
+    <!-- End Google Tag Manager -->
+
     <!-- Favicons-->
     <link rel="shortcut icon" href="{{ mix('img/favicon/favicon.ico') }}" type="image/x-icon">
     <link rel="apple-touch-icon" sizes="57x57" href="{{ mix('img/favicon/apple-icon-57x57.png') }}">
@@ -43,6 +51,10 @@
     <livewire:styles>
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M74WKHN"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 @yield('fb')
 <header class="header clearfix element_to_stick">
     @include('layouts.header')
